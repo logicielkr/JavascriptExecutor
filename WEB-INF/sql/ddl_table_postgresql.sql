52611f4d8f13d36a1d2da42ce6be475452f77061
logicielkr <graha@graha.kr>
2022-02-23 10:46:25 +0900

    1. schema 를 javascript로 변경
    - 반대로 변경하는 것이 더 쉽기 때문
    - PostgreSQL 의 schema는 Oracle의 그것과 달라서,
    - PostgreSQL의 경우 schema를 적절히 나누어서 관리하는 것이 효율적일 가능성이 높음.

--- WEB-INF/sql/ddl_table_postgresql.sql
+++ WEB-INF/sql/ddl_table_postgresql.sql
@@ -1,7 +1,9 @@
-CREATE SEQUENCE "javascript$javascript_id"; 
+CREATE SCHEMA javascript;
 
-create table javascript (
-	javascript_id integer NOT NULL DEFAULT nextval('javascript$javascript_id'::regclass),
+CREATE SEQUENCE javascript."javascript$javascript_id"; 
+
+create table javascript.javascript (
+	javascript_id integer NOT NULL DEFAULT nextval('javascript.javascript$javascript_id'::regclass),
 	title varchar(1000),
 	source text,
 	contents text,
@@ -20,30 +22,30 @@ create table javascript (
 /**
 like 검색 성능 향상
 */
-CREATE EXTENSION pg_trgm;
-CREATE INDEX "javascript$title" ON javascript USING gin (title gin_trgm_ops);
-CREATE INDEX "javascript$contents" ON javascript USING gin (contents gin_trgm_ops);
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+CREATE INDEX "javascript$title" ON javascript.javascript USING gin (title gin_trgm_ops);
+CREATE INDEX "javascript$contents" ON javascript.javascript USING gin (contents gin_trgm_ops);
 
-comment on table javascript is 'Javascript 실행기';
+comment on table javascript.javascript is 'Javascript 실행기';
 
-COMMENT ON COLUMN javascript.javascript_id IS '고유번호';
-COMMENT ON COLUMN javascript.title IS '제목';
-COMMENT ON COLUMN javascript.source IS '소스';
-COMMENT ON COLUMN javascript.contents IS '내용';
-COMMENT ON COLUMN javascript.results IS '실행결과';
-COMMENT ON COLUMN javascript.encrypted IS '암호화';
-COMMENT ON COLUMN javascript.parent_id IS '상위 Javascript 실행기 고유번호';
-COMMENT ON COLUMN javascript.insert_date IS '최초입력일시';
-COMMENT ON COLUMN javascript.insert_id IS '최초입력자ID';
-COMMENT ON COLUMN javascript.insert_ip IS '최초입력자IP';
-COMMENT ON COLUMN javascript.update_date IS '최종수정일시';
-COMMENT ON COLUMN javascript.update_id IS '최종수정자ID';
-COMMENT ON COLUMN javascript.update_ip IS '최종수정자IP';
+COMMENT ON COLUMN javascript.javascript.javascript_id IS '고유번호';
+COMMENT ON COLUMN javascript.javascript.title IS '제목';
+COMMENT ON COLUMN javascript.javascript.source IS '소스';
+COMMENT ON COLUMN javascript.javascript.contents IS '내용';
+COMMENT ON COLUMN javascript.javascript.results IS '실행결과';
+COMMENT ON COLUMN javascript.javascript.encrypted IS '암호화';
+COMMENT ON COLUMN javascript.javascript.parent_id IS '상위 Javascript 실행기 고유번호';
+COMMENT ON COLUMN javascript.javascript.insert_date IS '최초입력일시';
+COMMENT ON COLUMN javascript.javascript.insert_id IS '최초입력자ID';
+COMMENT ON COLUMN javascript.javascript.insert_ip IS '최초입력자IP';
+COMMENT ON COLUMN javascript.javascript.update_date IS '최종수정일시';
+COMMENT ON COLUMN javascript.javascript.update_id IS '최종수정자ID';
+COMMENT ON COLUMN javascript.javascript.update_ip IS '최종수정자IP';
 
-CREATE SEQUENCE "javascript_history$javascript_history_id";
+CREATE SEQUENCE javascript."javascript_history$javascript_history_id";
 
-create table javascript_history (
-	javascript_history_id integer NOT NULL DEFAULT nextval('javascript_history$javascript_history_id'::regclass),
+create table javascript.javascript_history (
+	javascript_history_id integer NOT NULL DEFAULT nextval('javascript.javascript_history$javascript_history_id'::regclass),
 	title varchar(1000),
 	source text,
 	contents text,
@@ -61,21 +63,21 @@ create table javascript_history (
 	PRIMARY KEY (javascript_history_id)
 );
 
-comment on table javascript_history is 'Javascript 실행기 이력관리';
+comment on table javascript.javascript_history is 'Javascript 실행기 이력관리';
 
-COMMENT ON COLUMN javascript_history.javascript_history_id IS '고유번호';
-COMMENT ON COLUMN javascript_history.title IS '제목';
-COMMENT ON COLUMN javascript_history.source IS '소스';
-COMMENT ON COLUMN javascript_history.contents IS '내용';
-COMMENT ON COLUMN javascript_history.results IS '실행결과';
-COMMENT ON COLUMN javascript_history.encrypted IS '암호화';
-COMMENT ON COLUMN javascript_history.parent_id IS '상위 Javascript 실행기 고유번호';
-COMMENT ON COLUMN javascript_history.javascript_id IS 'Javascript 실행기 ID';
-COMMENT ON COLUMN javascript_history.autosave IS '자동저장';
-COMMENT ON COLUMN javascript_history.insert_date IS '최초입력일시';
-COMMENT ON COLUMN javascript_history.insert_id IS '최초입력자ID';
-COMMENT ON COLUMN javascript_history.insert_ip IS '최초입력자IP';
-COMMENT ON COLUMN javascript_history.update_date IS '최종수정일시';
-COMMENT ON COLUMN javascript_history.update_id IS '최종수정자ID';
-COMMENT ON COLUMN javascript_history.update_ip IS '최종수정자IP';
+COMMENT ON COLUMN javascript.javascript_history.javascript_history_id IS '고유번호';
+COMMENT ON COLUMN javascript.javascript_history.title IS '제목';
+COMMENT ON COLUMN javascript.javascript_history.source IS '소스';
+COMMENT ON COLUMN javascript.javascript_history.contents IS '내용';
+COMMENT ON COLUMN javascript.javascript_history.results IS '실행결과';
+COMMENT ON COLUMN javascript.javascript_history.encrypted IS '암호화';
+COMMENT ON COLUMN javascript.javascript_history.parent_id IS '상위 Javascript 실행기 고유번호';
+COMMENT ON COLUMN javascript.javascript_history.javascript_id IS 'Javascript 실행기 ID';
+COMMENT ON COLUMN javascript.javascript_history.autosave IS '자동저장';
+COMMENT ON COLUMN javascript.javascript_history.insert_date IS '최초입력일시';
+COMMENT ON COLUMN javascript.javascript_history.insert_id IS '최초입력자ID';
+COMMENT ON COLUMN javascript.javascript_history.insert_ip IS '최초입력자IP';
+COMMENT ON COLUMN javascript.javascript_history.update_date IS '최종수정일시';
+COMMENT ON COLUMN javascript.javascript_history.update_id IS '최종수정자ID';
+COMMENT ON COLUMN javascript.javascript_history.update_ip IS '최종수정자IP';
 
